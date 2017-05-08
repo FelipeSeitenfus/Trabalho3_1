8076eaed56a3d242d5d992dd4750167ac1d2a1fa
GregoryMarchesan <gregory.marchesan@ecomp.ufsm.br>
2017-05-07 22:38:42 -0300

    Update crypto_interrupt.asm

--- crypto_interrupt.asm
+++ crypto_interrupt.asm
@@ -11,7 +11,8 @@
 .org #0000h             ; Code start
 
 .code
-; ----------------------- INICIALIZAÇÕES DAS PORTAS, ENDEREÇOS E BITMASKS -------------------
+; ----------------------- INICIALIZAÇÕES DAS PORTAS E ENDEREÇOS -------------------
+boot:	
 	ldh r0, #01h
 	ldl r0, #AEh ;
 	ldsp r0 ; SP <= 01AEh
@@ -53,40 +54,39 @@
 	st r5, r15, r0 ; address_PortData_B
 	
 	ldh r15, #60h
-	ldl r15, #FFh ; PortConfig <= 0111000011111111
-	st r15, r2, r0 ; PortConfigA <= 0111000011111111
+	ldl r15, #FFh ; PortConfig <= 0110000011111111
+	st r15, r2, r0 ; PortConfigA <= 0110000011111111
 	
 	ldh r15, #74h
-	ldl r15, #FFh ; PortConfig <= 0111000011111111
+	ldl r15, #FFh ; PortConfig <= 0111010011111111
 	st r15, r5, r0 ; PortConfigB <= 0111010011111111
 	
 	st r0, r1, r0 ; PortData_A <= 0000h
 	st r0, r4, r0 ; PortData_B <= 0000h
 	
-	ldh r15, #24h
-	ldl r15, #00h
-	st r15, r7, r0 ;irqEnable port_B
+	ldh r15, #24h ; 00100100
+	ldl r15, #00h ;
+	st r15, r7, r0 ; irqEnable_PortB
 	
 	; enable PortA and PortB
 	ldh r15, #98h
-	st r15, r3, r0 ; PortEnable_A <= 1111100011111111
-	
+	ldl r15, #FFh
+	st r15, r3, r0 ; PortEnable_A <= 1101100011111111
 	ldh r15, #FCh
-	st r15, r6, r0 ; PortEnable_B <= 1111100011111111
-	
+	st r15, r6, r0 ; PortEnable_B <= 1111110011111111
+
+	; RANDOM NUMBER INICIALIZATION
 	ldh r15, #random_number
 	ldl r15, #random_number
-	
-	; r3 is free
 	xor r3, r3, r3 ; r3 <= 0 ; random number
-	st r3, r15, r0
-	; bitmasks inicialization 
+	st r3, r15, r0 ; random_number
+	
+	
 	
 ; ---------------------------------------- INICIO CRYPTO1 ---------------------------------------------	
 
 inicio_crypto1:
-	;carregar endereços dos regs 
-	; arrumar tristate
+	; carregar endereços dos regs 
 	ldh r1, #address_PortData_A
 	ldl r1, #address_PortData_A
 	ld r1, r1, r0 ; r1 <= address_PortData_A
@@ -97,19 +97,20 @@ inicio_crypto1:
 	
 	ldh r3, #random_number
 	ldl r3, #random_number
-	ld r3, r3, r0
+	ld r3, r3, r0 ; r3 <= random_number
 	jsrd #testa_crypto
-	jsrd #calcula_chave
+	jsrd #calcula_chave ; chave em r7 ou em #chave
 	
 	xor r6, r6, r6 ; r6 <= contador de caracteres
+	
 	jsrd #le_caractere_crypto
 	and r4, r15, r4 ; verifica fim da mensagem
 	jmpzd #move_data_up_crypto1
 	jmpd #guarda_caractere_crypto1
 	
 move_data_up_crypto1:
-	jsrd #move_data_up_crypto;
-	
+	jsrd #move_data_up_crypto
+
 le_caractere2_crypto1: ; r8 <= caractere par
 	ld r4, r1, r0 ; r4 <= PortData_A
 	and r5, r14, r4
@@ -145,98 +146,6 @@ inicio_crypto2:
 	ldl r2, #address_PortConfig_B
 	ld r2, r2, r0 ; r2 <= address_PortConfig_B
 	
-testa_crypto_2:
-	ld r8, r1, r0 ; r8 <= PortData_B
-	and r4, r8, r9 
-	JMPZD #testa_crypto_2 ; pooling enquanto keyExchange = 0
-	and r8, r8, r10 ; BITS do magic number do Crypto
-	ldh r4, #magicNumberFromCrypto2
-	ldl r4, #magicNumberFromCrypto2
-	st r8, r4, r0 ; magicNumberFromCrypto1 <= magicNumberFromCrypto1
-calcula_magic_number2:
-	; prepara para a chamada de exp_mod (parametros)
-	ldh r5, #00h
-	ldl r5, #FBh ; q <= 251
-	ldh r6, #00h
-	ldl r6, #06h ; a <= 6
-	jsrd #verifica_num_alet
-	addi r3, #1 ; incrementa o numero aleatorio
-	jsrd #exp_mod ; resposta retornada em r7 => magicNumberFromProcessor
-	ldh r4, #magicNumberFromProcessor
-	ldl r4, #magicNumberFromProcessor
-	st r7, r4, r0 ; magicNumberFromProcessor <= r7
-; magicNumber do processador em r7
-	ld r4, r1, r0
-	or r4, r4, r13 ; seta o bit para o tristate
-	st r4, r1, r0 
-
-	ld r4, r2, r0 ; r4 <= PortConfigA
-	and r4, r4, r11 ; r4 <= Porta vira saida
-	st r4, r2, r0 ; PortA(7:0) => saida
-	
-	or r5, r7, r13 ; seta o magicNumber e o bit do tristate
-	or r5, r5, r12 ; seta ack
-	st r5, r1, r0 ; PortData_A <= MagicNumber + ack + tristate_signal
-	xor r5, r5, r12 ; desativa o ack
-	st r5, r1, r0 ; desativa o ack
-	
-	ld r4, r2, r0 ; r4 <= PortConfigA
-	or r4, r10, r4 ; 
-	st r4, r2, r0 ; PortA(7:0) => entrada
-	xor r4, r4, r4 ;
-	st r4, r1, r0 ; desativa o tristate
-calcula_chave_2:
-	ldh r4, #magicNumberFromCrypto2
-	ldl r4, #magicNumberFromCrypto2
-	ld r6, r4, r0 ; r6 <= MagicNumber do crypto A
-	ldh r5, #00h
-	ldl r5, #FBh ; r5 <= q (251)
-	jsrd #exp_mod ; chave em r7
-	xor r6, r6, r6 ; r6 <= contador de caracteres
-le_caractere1_crypto2:
-	ld r4, r1, r0 ; r4 <= PortData_A
-	and r5, r4, r9 ; teste de mensagem vazia
-	jmpzd #mensagem_nao_vazia_crypto2 ;
-	jmpd #inicio_crypto1 ; mensagem vazia, logo recomeca pelo outro crypto 
-mensagem_nao_vazia_crypto2:
-	and r5, r14, r4
-	JMPZD #le_caractere1_crypto2 ; pooling enquanto o caractere nao esta pronto
-	or r5, r4, r12 ;
-	st r5, r1, r0 ; pulso em ack
-	xor r5, r5, r12 ;
-	st r5, r1, r0 ; limpa o ack
-	and r8, r4, r10 ; limpa a parte alta de PortDataA
-	xor r8, r7, r8 ; descriptografa a mensagem
-	; mensagem descriptografa em r8 (parte baixa)
-	and r4, r15, r4 ; verifica fim da mensagem
-	jmpzd #move_data_up_crypto2
-	jmpd #guarda_caractere_crypto2
-move_data_up_crypto2:
-	jsrd #move_high ; r8 <= caractere (parte alta)
-le_caractere2_crypto2: ; r8 <= caractere par
-	ld r4, r1, r0 ; r4 <= PortData_A
-	and r5, r14, r4
-	JMPZD #le_caractere2_crypto2 ; pooling enquanto o caractere nao esta pronto
-	or r5, r4, r12 ;
-	st r5, r1, r0 ; pulso em ack
-	xor r5, r5, r12 ;
-	st r5, r1, r0 ; limpa o ack
-	and r5, r4, r10 ; limpa a parte alta de PortDataA
-	xor r5, r7, r5 ; descriptografa a mensagem
-arruma_data_crypto2: ; parte baixa da mensagem pronta em r5
-	or r8, r8, r5 ; mensagem pronta para gravar em r8
-guarda_caractere_crypto2:
-	ldh r5, #msg_c2 ;
-	ldl r5, #msg_c2 ; r5 <= ponteiro para a variavel
-	st r8, r5, r6 ; grava na memória
-	addi r6, #1 ; r6++
-	and r4, r15, r4 ; verifica fim da mensagem
-	jmpzd #le_caractere1_crypto2
-
-	jmpd #inicio_crypto1 ; reinicia a leitura das mensagens
-	
-; ---------------------------------- FIM CRYPTO2 --------------------------------------------------
-
 ; ---------------------------------- FUNÇÕES GERAIS DA APLICAÇÃO ----------------------------------	
 	
 ; finds a^b mod q 
@@ -304,9 +213,8 @@ reinicia_numero:
 		ldl r3, #00h
 		pop r5
 		rts
-bitmask_init:	
-	ldh r9, #20h
-	ldl r9, #00h ; r9 <= bitmask para keyExchange 
+		
+bitmask_init:
 	ldh r10, #00h
 	ldl r10, #FFh ; r10 <= bitmask para data_in(crypto) -- LOW BYTE MASK
 	ldh r11, #FFh
@@ -318,9 +226,10 @@ bitmask_init:
 	ldh r14, #40h
 	ldl r14, #00h ; r14 <= bitmask para data_av 
 	ldh r15, #10h
-	ldl r15, #00h ; r15 <= bitmask para eom
+	ldl r15, #00h ; r15 <= bitmask para eom 
 	rts
-
+	
+; magicNumber retornado em r7 e guardado em magicNumberFromProcessor
 calcula_magic_number:
 	; prepara para a chamada de exp_mod (parametros)
 	ldh r5, #00h
@@ -333,7 +242,7 @@ calcula_magic_number:
 	ldh r4, #magicNumberFromProcessor
 	ldl r4, #magicNumberFromProcessor
 	st r7, r4, r0 ; magicNumberFromProcessor <= r7
-; magicNumber do processador em r7
+	; magicNumber do processador em r7
 	ld r4, r1, r0
 	or r4, r4, r13 ; seta o bit para o tristate
 	st r4, r1, r0 
@@ -355,11 +264,13 @@ calcula_magic_number:
 	st r4, r1, r0 ; desativa o tristate
 	rts
 	
+; pega o magicNumber do crypto e calcula o magicNumber do processador
 testa_crypto:
 	and r8, r8, r10 ; BITS do magic number do Crypto
 	ldh r4, #magicNumberFromCrypto
 	ldl r4, #magicNumberFromCrypto
-	st r8, r4, r0 ; magicNumberFromCrypto1 <= magicNumberFromCrypto1
+	st r8, r4, r0 ; magicNumberFromCrypto <= magicNumberFromCrypto
+	; calcula_magic_number
 	jsrd #calcula_magic_number
 	rts
 	
@@ -374,9 +285,9 @@ calcula_chave:
 	ldl r4, #chave
 	st r7, #chave
 	rts
-
+	
 le_caractere_crypto:
-	ld r4, r1, r0 ; r4 <= PortData_A
+	ld r4, r1, r0  ; r4 <= PortData_A
 	and r5, r14, r4
 	JMPZD #le_caractere_crypto ; pooling enquanto o caractere nao esta pronto
 	or r5, r4, r12 ;
@@ -387,17 +298,16 @@ le_caractere_crypto:
 	xor r8, r7, r8 ; descriptografa a mensagem
 	ldh r5, #buffer
 	ldl r5, #buffer
-	st r8, r5, r0 ; buffer <= mensagem descriptografada
-	rts
+	st r8, r5, r0 ; buffer <= mensagem descriptografa
+	rts	
 	
 move_data_up_crypto:
 	ldh r5, #buffer
 	ldl r5, #buffer
-	ld r8, r5, r0
+	ld r8, r5, r0 ; r8 <= buffer
 	jsrd #move_high ; r8 <= caractere (parte alta)
-	st r8, r5, r0
+	st r8, r5, r0 ; buffer <= r8 << 8
 	rts
-	
 .endcode
 
 ; Data area (variables)
