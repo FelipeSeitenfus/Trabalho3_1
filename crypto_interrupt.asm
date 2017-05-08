cbb8a627dd4b757830fe78c7fd67c6ee97bba43b
GregoryMarchesan <gregory.marchesan@ecomp.ufsm.br>
2017-05-07 23:31:41 -0300

    Update crypto_interrupt.asm

--- crypto_interrupt.asm
+++ crypto_interrupt.asm
@@ -80,13 +80,73 @@ boot:
 	ldl r15, #random_number
 	xor r3, r3, r3 ; r3 <= 0 ; random number
 	st r3, r15, r0 ; random_number
+	jmpd #BubbleSort
 	
+InterruptionServiceRoutine:
+	; salvamento de contexto
+	push r0
+	push r1
+	push r2
+	push r3
+	push r4
+	push r5
+	push r6
+	push r7
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+	pushf 
 	
+	; identificação da origem da interrupcao
+	ldh r1, #20h  ; 
+	ldl r1, #00h  ; r1 <= 0010 0000 0000 0000
+	ldh r2, #90h
+	ldl r2, #00h  ; r2 <= PortDataB_ADDR
+	ld r3, r2, r0 ; r3 <= PortData_B
+	
+	and r4, r3, r1
+	jmpzd #Crypto1_handler
+	jmpd #Crypto2_handler
+	
+Crypto1_handler:
+	jsrd #inicio_crypto1 
+	jmpd #recupera_contexto
+	
+Crypto2_handler:
+	jsrd #inicio_crypto2
+	
+	; recuperacao de contexto
+recupera_contexto:
+	pop r0
+	pop r1
+	pop r2
+	pop r3
+	pop r4
+	pop r5
+	pop r6
+	pop r7
+	pop r8
+	pop r9
+	pop r10
+	pop r11
+	pop r12
+	pop r13
+	pop r14
+	pop r15
+	popf 
+	
+	rti
 	
 ; ---------------------------------------- INICIO CRYPTO1 ---------------------------------------------	
 
 inicio_crypto1:
 	; carregar endereços dos regs 
+	jsrd #bitmask_init
 	ldh r1, #address_PortData_A
 	ldl r1, #address_PortData_A
 	ld r1, r1, r0 ; r1 <= address_PortData_A
@@ -102,42 +162,40 @@ inicio_crypto1:
 	jsrd #calcula_chave ; chave em r7 ou em #chave
 	
 	xor r6, r6, r6 ; r6 <= contador de caracteres
-	
-	jsrd #le_caractere_crypto
-	and r4, r15, r4 ; verifica fim da mensagem
+le_caractere_crypto1:
+	ldl r4, #buffer_caract
+	ldh r4, #buffer_caract
+	st r0, r4, r0 ; zera o buffer_caract
+	jsrd #le_caractere_crypto ; le o caractere 1
+	jsrd #verifica_fim_mensagem
 	jmpzd #move_data_up_crypto1
 	jmpd #guarda_caractere_crypto1
 	
 move_data_up_crypto1:
 	jsrd #move_data_up_crypto
-
-le_caractere2_crypto1: ; r8 <= caractere par
-	ld r4, r1, r0 ; r4 <= PortData_A
-	and r5, r14, r4
-	JMPZD #le_caractere2_crypto1 ; pooling enquanto o caractere nao esta pronto
-	or r5, r4, r12 ;
-	st r5, r1, r0 ; pulso em ack
-	xor r5, r5, r12 ;
-	st r5, r1, r0 ; limpa o ack
-	and r5, r4, r10 ; limpa a parte alta de PortDataA
-	xor r5, r7, r5 ; descriptografa a mensagem
-arruma_data_crypto1: ; parte baixa da mensagem pronta em r5
-	or r8, r8, r5 ; mensagem pronta para gravar em r8
+	
+	jsrd #le_caractere_crypto ; le o caractere2
+	
 guarda_caractere_crypto1:
 	ldh r5, #msg_c1 ;
 	ldl r5, #msg_c1 ; r5 <= ponteiro para a variavel
+	ldh r4, #buffer_caract
+	ldl r4, #buffer_caract
+	ld r8, r4, r0 ; r8 <= buffer_caract
 	st r8, r5, r6 ; grava na memória
 	addi r6, #1 ; r6++
-	and r4, r15, r4 ; verifica fim da mensagem
-	jmpzd #le_caractere1_crypto1
+	jsrd #verifica_fim_mensagem
+	jmpzd #le_caractere_crypto1
 fim_mensagem_crypto_1: 
+	rts
 	
 ; ------------------------------------- FIM CRYPTO1 -------------------------------------------------
 
 
 ; ------------------------------------- INICIO CRYPTO2 ----------------------------------------------
 inicio_crypto2:
-	;carregar endereços dos regs 
+	; carregar endereços dos regs 
+	jsrd #bitmask_init
 	ldh r1, #address_PortData_B
 	ldl r1, #address_PortData_B
 	ld r1, r1, r0 ; r1 <= address_PortData_B
@@ -146,6 +204,40 @@ inicio_crypto2:
 	ldl r2, #address_PortConfig_B
 	ld r2, r2, r0 ; r2 <= address_PortConfig_B
 	
+	ldh r3, #random_number
+	ldl r3, #random_number
+	ld r3, r3, r0 ; r3 <= random_number
+	jsrd #testa_crypto
+	jsrd #calcula_chave ; chave em r7 ou em #chave
+	
+	xor r6, r6, r6 ; r6 <= contador de caracteres
+	
+le_caractere_crypto2:
+	ldl r4, #buffer_caract
+	ldh r4, #buffer_caract
+	st r0, r4, r0 ; zera o buffer_caract
+	jsrd #le_caractere_crypto ; le o caractere 1
+	jsrd #verifica_fim_mensagem
+	jmpzd #move_data_up_crypto2
+	jmpd #guarda_caractere_crypto2
+	
+move_data_up_crypto2:
+	jsrd #move_data_up_crypto
+	
+	jsrd #le_caractere_crypto ; le o caractere2
+	
+guarda_caractere_crypto2:
+	ldh r5, #msg_c2 ;
+	ldl r5, #msg_c2 ; r5 <= ponteiro para a variavel
+	ldh r4, #buffer_caract
+	ldl r4, #buffer_caract
+	ld r8, r4, r0 ; r8 <= buffer_caract
+	st r8, r5, r6 ; grava na memória
+	addi r6, #1 ; r6++
+	jsrd #verifica_fim_mensagem
+	jmpzd #le_caractere_crypto2
+fim_mensagem_crypto_2: 
+	rts
 ; ---------------------------------- FUNÇÕES GERAIS DA APLICAÇÃO ----------------------------------	
 	
 ; finds a^b mod q 
@@ -232,6 +324,9 @@ bitmask_init:
 ; magicNumber retornado em r7 e guardado em magicNumberFromProcessor
 calcula_magic_number:
 	; prepara para a chamada de exp_mod (parametros)
+	ldh r3, #random_number
+	ldl r3, #random_number
+	ld r3, r3, r0 ; r3 <= random_number
 	ldh r5, #00h
 	ldl r5, #FBh ; q <= 251
 	ldh r6, #00h
@@ -275,6 +370,9 @@ testa_crypto:
 	rts
 	
 calcula_chave:
+	ldh r3, #random_number
+	ldl r3, #random_number
+	ld r3, r3, r0 ; r3 <= random_number
 	ldh r4, #magicNumberFromCrypto
 	ldl r4, #magicNumberFromCrypto
 	ld r6, r4, r0 ; r6 <= MagicNumber do crypto A
@@ -283,31 +381,102 @@ calcula_chave:
 	jsrd #exp_mod ; chave em r7
 	ldh r4, #chave
 	ldl r4, #chave
-	st r7, #chave
+	st r7, r4, r0
 	rts
 	
 le_caractere_crypto:
 	ld r4, r1, r0  ; r4 <= PortData_A
 	and r5, r14, r4
 	JMPZD #le_caractere_crypto ; pooling enquanto o caractere nao esta pronto
+	ldh r5, #buffer_leitura
+	ldl r5, #buffer_leitura
+	st r4, r5, r0 ; buffer_leitura <= r4
 	or r5, r4, r12 ;
 	st r5, r1, r0 ; pulso em ack
 	xor r5, r5, r12 ;
 	st r5, r1, r0 ; limpa o ack
 	and r8, r4, r10 ; limpa a parte alta de PortDataA
 	xor r8, r7, r8 ; descriptografa a mensagem
-	ldh r5, #buffer
-	ldl r5, #buffer
-	st r8, r5, r0 ; buffer <= mensagem descriptografa
+	ldh r5, #buffer_caract
+	ldl r5, #buffer_caract
+	ld r4, r5, r0 ; r4 <= buffer_caract
+	or r8, r4, r8 ; r8 <= r4 or r8
+	st r8, r5, r0 ; buffer <= mensagem descriptografada
 	rts	
 	
 move_data_up_crypto:
-	ldh r5, #buffer
-	ldl r5, #buffer
-	ld r8, r5, r0 ; r8 <= buffer
+	ldh r5, #buffer_caract
+	ldl r5, #buffer_caract
+	ld r8, r5, r0 ; r8 <= buffer_caract
 	jsrd #move_high ; r8 <= caractere (parte alta)
-	st r8, r5, r0 ; buffer <= r8 << 8
+	st r8, r5, r0 ; buffer_caract <= r8 << 8
 	rts
+	
+verifica_fim_mensagem:
+	ldh r5, #buffer_leitura
+	ldl r5, #buffer_leitura
+	ld r4, r5, r0 ; r4 <= buffer_leitura
+	and r4, r15, r4 ; verifica fim da mensagem
+	rts
+	
+; ----------------------------- BUBBLESORT ---------------------------------------------------------	
+BubbleSort:
+   
+    ; Initialization code
+    xor r0, r0, r0          ; r0 <- 0
+    
+    ldh r4, #array          ;
+    ldl r4, #array          ; r4 <- &array
+    
+    ldh r2, #size           ;
+    ldl r2, #size           ; r2 <- &size
+    ld r2, r2, r0           ; r2 <- size
+    
+    add r3, r2, r4          ; r3 points the end of array (right after the last element)
+    
+    ldl r4, #0              ;
+    ldh r4, #4              ; r4 <- 4
+   
+   
+; Main code
+scan:
+    addi r4, #0             ; Verifies if there was element swaping
+    jmpzd #end              ; If r4 = 0 then no element swaping
+    
+    xor r4, r4, r4          ; r4 <- 0 before each pass
+    
+    add r5, r4, r0          ; r5 points the first arrar element
+    
+    add r6, r4, r0          ;
+    addi r6, #4             ; r6 points the second array element
+    
+; Read two consecutive elements and compares them    
+loop_bubble:
+    ld r7, r5, r0           ; r7 <- array[r5]
+    ld r8, r6, r0           ; r8 <- array[r6]
+    sub r2, r8, r7          ; If r8 > r7, negative flag is set
+    jmpnd #swap             ; (if array[r5] > array[r6] jump)
+    
+; Increments the index registers and verifies is the pass is concluded
+continue:
+    addi r5, #4             ; r5++
+    addi r6, #4             ; r6++
+    
+    sub r2, r6, r3          ; Verifies if the end of array was reached (r6 = r3)
+    jmpzd #scan             ; If r6 = r3 jump
+    jmpd #loop_bubble              ; else, the next two elements are compared
+
+
+; Swaps two array elements (memory)
+swap:
+    st r7, r6, r0           ; array[r6] <- r7
+    st r8, r5, r0           ; array[r5] <- r8
+    ldl r4, #4              ; Set the element swaping (r4 <- 4)
+    jmpd #continue
+        
+end:    
+    halt                    ; Suspend the execution
+	
 .endcode
 
 ; Data area (variables)
@@ -316,11 +485,16 @@ move_data_up_crypto:
 	address_PortData_B: db #00h
 	address_PortConfig_A: db #00h
 	address_PortConfig_B: db #00h
-	buffer: db #00h
+	buffer_caract: db #00h
+	buffer_leitura: db #00h
 	random_number: db #00h
 	chave: db #00h
 	magicNumberFromProcessor: db #00h
 	magicNumberFromCrypto: db #00h
 	msg_c1: db #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h
     msg_c2: db #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h, #00h
+	
+	array:     db #50h, #49h, #48h, #47h, #46h, #45h, #44h, #43h, #42h, #41h, #40h, #39h, #38h, #37h, #36h, #35h, #34h, #33h, #32h, #31h, #30h, #29h, #28h, #27h, #26h, #25h, #24h, #23h, #22h, #21h, #20h, #19h, #18h, #17h, #16h, #15h, #14h, #13h, #12h, #11h, #10h, #9h, #8h, #7h, #6h, #5h, #4h, #3h, #2h, #4h, #20h, #49h, #48h, #47h, #46h, #45h, #44h, #43h, #42h, #44h, #40h, #9h, #8h, #7h, #6h, #5h, #4h, #3h, #2h, #1h
+    size:      db #50    ; 'array' size  
+
 .enddata
